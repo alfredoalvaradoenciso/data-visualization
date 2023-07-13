df880262ffa4d00881024e0db7f50439702d5105
alfredoalvaradoenciso <91037488+alfredoalvaradoenciso@users.noreply.github.com>
2023-07-13 13:59:00 -0400

    treemap

--- treemap.R
+++ treemap.R
@@ -1,33 +1,31 @@
 
 library(xlsx)
-data <- read.xlsx("C:/Users/Dell/Downloads/vacantes.xlsx", sheetIndex = 1, stringsAsFactors = FALSE, encoding="UTF-8")
+data <- read.xlsx("C:/Users/Dell/Documents/GitHub/data-visualization/vacantes4.xlsx", sheetIndex = 1, stringsAsFactors = FALSE, encoding="UTF-8")
 
 library(xtable)
 library(dplyr)
 
 #Remove commas from numeric values in number columns
-data$c5_p9_3_1 <- gsub(",","", data$c5_p9_3_1)
+data$c5_p13 <- gsub(",","", data$c5_p13)
 
 #Convert numeric columns to a numeric data type
-data$c5_p9_3_1 <- as.numeric(data$c5_p9_3_1)
+data$c5_p13 <- as.numeric(data$c5_p13)
 
 #Create new data frame for positive (spending) values
-data$n2 <- paste(data$n, data$pc3, sep = ", ")
-spending <- data %>% select(ciuo1, n2, c5_p9_3_1) %>%
-  group_by(ciuo1, n2) %>%
-  summarize(c5_p9_3_1 = sum(c5_p9_3_1, na.rm=FALSE)) %>%
-  filter(c5_p9_3_1 > 0)
+data$n2 <- paste(data$ciuo1, data$pc4, sep = ", ")
+spending <- data %>% select(n, n2, c5_p13) 
 
 library(treemap)
 treemap(spending, #Your data frame object
-        index=c("ciuo1","n2"),  #A list of your categorical variables
-        vSize = "c5_p9_3_1",  #This is your quantitative variable
+        index="n2",  #A list of your categorical variables
+        vSize = "c5_p13",  #This is your quantitative variable
         type="index", #Type sets the organization and color scheme of your treemap
-        palette = "-RdBu",  #Select your color palette from the RColorBrewer presets or make your own.
-        title="Vacantes requeridas seg?n grupos de ocupaciones", #Customize your title
+        palette = "-Set3",  #Select your color palette from the RColorBrewer presets or make your own.
+        title="", #Customize your title
         fontsize.title = 14, #Change the font size of the title
-inflate.labels = TRUE,
- fontsize.labels=c(0,1),
-lowerbound.cex.labels=1,       
-force.print.labels=FALSE
+        inflate.labels = TRUE,
+        fontsize.labels=1,
+        lowerbound.cex.labels=1,       
+        force.print.labels=TRUE,
+        overlap.labels = 1
 )
